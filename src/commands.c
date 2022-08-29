1d17d9269895950e5bfcc9c80fbd8723f6b946ee
VMpc <bdfa5ifn@anonaddy.me>
2022-08-29 06:06:38 -0400

    Suggestion by StrafeUltra & change sleep to 100ms

--- src/commands.c
+++ src/commands.c
@@ -10,21 +10,17 @@
 
 #include <string.h>
 
-#define NOP (char)0x90
-
 char *toggleEsp(Game *game, char **args) {
-  char NOPs[7] = {NOP, NOP, NOP, NOP, NOP, NOP, (char)0xFF};
-
-  char Revert[8] = {(char)0x0F, (char)0x83, (char)0xD1, (char)0x01,
-                    (char)0x00, (char)0x00, (char)0xFF, NOP};
+  char *NOPs = "\x90\x90\x90\x90\x90\x90\xFF";
+  char *Revert = "\x0F\x83\xD1\x01\x00\x00\x0FF\x90";
 
   if (strstr(args[1], "on")) {
     pokeAddr(game->pid, game->clientModule + Offsets.GlowAddr, NOPs,
-              sizeof(NOPs));
+             strlen(NOPs));
     return "Turned on esp";
   } else if (strstr(args[1], "off")) {
     pokeAddr(game->pid, game->clientModule + Offsets.GlowAddr, Revert,
-              sizeof(Revert));
+             strlen(Revert));
     return "Turned off esp";
   }
 
@@ -32,18 +28,16 @@ char *toggleEsp(Game *game, char **args) {
 }
 
 char *toggleNoBoom(Game *game, char **args) {
-  char NOPs[8] = {NOP, NOP, NOP, NOP, NOP, (char)0x89, (char)0x1C, (char)0x24};
-
-  char Revert[8] = {(char)0xE8, (char)0x46, (char)0x2B, (char)0xFF,
-                    (char)0xFF, (char)0x89, (char)0x1C, (char)0x24};
+  char *NOPs = "\x90\x90\x90\x90\x90\x89\x1C\x24";
+  char *Revert = "\xE8\x46\x2B\xFF\xFF\x89\x1C\x24";
 
   if (strstr(args[1], "on")) {
     pokeAddr(game->pid, game->clientModule + Offsets.BoomAddr, NOPs,
-              sizeof(NOPs));
+             strlen(NOPs));
     return "Turned on no-boom";
   } else if (strstr(args[1], "off")) {
     pokeAddr(game->pid, game->clientModule + Offsets.BoomAddr, Revert,
-              sizeof(Revert));
+             strlen(Revert));
     return "Turned off no-boom";
   }
 
